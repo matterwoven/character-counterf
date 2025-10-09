762d8b84ecb36954e661662f6641a3669a5de007
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2025-10-09 14:12:25 -0700

    Class work until pc died

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -1,7 +1,38 @@
 import static org.junit.Assert.assertEquals;
 
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
+
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
     
+    @Test
+    public void testRepeatedLetterInStringFrequency() {
+            //Arrange
+            CharacterFrequencyCounter counter = new CharacterFrequencyCounter("Hello");
+            //Act
+            int actual = counter.getFrequency('1');
+            //Assert
+            assertEquals(2, actual);
+    }
+
+    @Test
+    public void testSingleLetterInStringFrequency() {
+            //Arrange
+            CharacterFrequencyCounter counter = new CharacterFrequencyCounter("Hello");
+            //Act
+            int actual = counter.getFrequency('e');
+            //Assert
+            assertEquals(1, actual);
+    }
+    
+    @Test
+    public void testRelativeFrequencyOnlySpecialCharacters() {
+            //Arrange
+            CharacterFrequencyCounter counter = new CharacterFrequencyCounter("$%^&");
+            //Act
+            int actual = counter.getRelativePercentage('1');
+            //Assert
+            assertEquals(0.0, actual, .001);
+    }
 }
