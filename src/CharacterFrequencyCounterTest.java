58aed0fb8f773f64a28d24743c4daf8a1b8b2e3d
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2026-04-16 11:01:01 -0700

    Changes thus far

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -2,6 +2,15 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    void testCharacterShowsUpMultipleTimesGivesCorrectFrequency() {
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        //Act
+        int actual = counter.getFrequency('l');
+        //Assert
+        assertEquals(2, actual);
+    }
 }
